98d264da989dfcda12badce83bc0a21c946627c2
Crowdbotics <team@crowdbotics.com>
2022-04-25 18:23:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gemtech_34918/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gemtech_34918;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gemtech_34918";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gemtech_34918",
+  url: "https://gemtech_34918.botics.co",
+  api: "https://gemtech_34918.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
